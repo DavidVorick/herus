81a8eb2162659a9d3e4c2b85de51971c2a6616e5
David Vorick <david.vorick@gmail.com>
2016-02-27 18:13:04 -0500

    add ability to connect pages

---
+++ templates/connect.tpl
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Upload Media</title>
+</head>
+
+<body>
+<form action="connect" method="post">
+	Source Topic: <input type="test" name="sourceTopic"><br>
+	Destination Topic: <input type="text" name="destinationTopic"></br>
+	<input type="submit" value="Connect"><br>
+</form>
+</body>
+</html>
--- topic.go
+++ topic.go
@@ -1,10 +1,9 @@
 package main
 
-// topic.go serves pages associated with the topics requested by users.
+// topic.go produces the topics page.
 
 import (
 	"encoding/json"
-	"fmt"
 	"html/template"
 	"net/http"
 	"strings"
@@ -16,11 +15,22 @@ const (
 	topicPrefix = "/t/"
 )
 
+// topicRelation is a mapping from one topic to another.
+type topicRelation struct {
+	TopicTitle string
+
+	CenterVotes uint64
+	Downvotes   uint64
+	LeftVotes   uint64
+	RightVotes  uint64
+	Upvotes     uint64
+}
+
 // topicData is a struct that gets stored in the database containing all of the
 // information about a topic.
 type topicData struct {
 	AssociatedMedia []mediaMetadata
-	// TODO: RelatedPages []pageRelations
+	RelatedTopics   []topicRelation
 }
 
 // topicTemplateData provides the dynamic data that is used to fill out the
@@ -30,18 +40,27 @@ type topicTemplateData struct {
 	TopicTitle  string
 
 	AssociatedMedia []mediaMetadata
+	RelatedTopics   []topicRelation
 }
 
 // topicHandler handles requests for topic pages.
 func (h *herus) topicHandler(w http.ResponseWriter, r *http.Request) {
-	topicName := strings.TrimPrefix(r.URL.Path, topicPrefix)
+	// Parse and preprocess the topic name.
+	//
+	// TODO: It's not really great... there's probably a better way to format
+	// topicTitle.
+	topicTitle := strings.TrimPrefix(r.URL.Path, topicPrefix)
+	topicTitle = strings.Replace(topicTitle, "_", " ", -1)
+	topicTitle = strings.ToTitle(topicTitle)
+	topicName := strings.ToLower(topicTitle)
+	topicName = strings.Replace(topicName, " ", "_", -1)
 
 	// Get a list of media from the database and build links to each media
 	// file.
 	var td topicData
 	err := h.db.View(func(tx *bolt.Tx) error {
-		tb := tx.Bucket(bucketTopics)
-		topicDataBytes := tb.Get([]byte(topicName))
+		bt := tx.Bucket(bucketTopics)
+		topicDataBytes := bt.Get([]byte(topicName))
 		if topicDataBytes != nil {
 			err := json.Unmarshal(topicDataBytes, &td)
 			if err != nil {
@@ -54,14 +73,14 @@ func (h *herus) topicHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Println(td)
 
 	// Fill out the stuct that will inform the topic template.
 	ttd := topicTemplateData{
 		MediaPrefix: mediaPrefix,
-		TopicTitle:  topicName,
+		TopicTitle:  topicTitle,
 
 		AssociatedMedia: td.AssociatedMedia,
+		RelatedTopics:   td.RelatedTopics,
 	}
 
 	// Execute a template to display all of the uploaded media.
