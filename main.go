4588e44f16ea69c36d7d1624171dfcc1820be996
David Vorick <david.vorick@gmail.com>
2016-03-06 15:45:10 -0500

    merge server.go and main.go

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -30,6 +32,40 @@ var (
 // of the server.
 type herus struct {
 	db *bolt.DB
+}
+
+// rootHandler will handle any request that comes to the page root - usually
+// indicating some type of malformed request or 404.
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles(filepath.Join(dirTemplates, "404.tpl"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+// establishServerRoutes writes all of the routes that are understandable to
+// the server.
+func (h *herus) establishServerRoutes() {
+	http.HandleFunc("/", rootHandler)
+
+	// Feed all of the web assets - must be accessible from /t/ and /m/ as
+	// well.
+	wafs := http.FileServer(http.Dir("web-assets"))
+	wafs = http.StripPrefix("/web-assets/", wafs)
+	http.Handle("/web-assets/", wafs)
+
+	http.Handle("/m/", http.StripPrefix("/m/", http.FileServer(http.Dir("media")))) // Serve everything directly from the media dir
+	http.HandleFunc(connectPage, h.connectHandler)
+	http.HandleFunc(elaborationPrefix, h.elaborationHandler)
+	http.HandleFunc(indexPage, indexHandler)
+	http.HandleFunc(topicPrefix, h.topicHandler)
+	http.HandleFunc(uploadPage, h.uploadHandler)
 }
 
 // initDB will initialize the database used by herus.
